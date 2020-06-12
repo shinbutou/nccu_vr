22bc7b62217ad0b2c7653ae77224a3cb56f69471
harrypao <107703037@nccu.edu.tw>
2020-06-12 20:07:26 +0800

    1st person functionalities added

---
+++ MouseLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    public float yRotation = 0.0f;
+    public float xRotation = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        yRotation += speedH * Input.GetAxis("Mouse X");
+        xRotation -= speedV * Input.GetAxis("Mouse Y");
+        if (xRotation >= 45f) xRotation = 45f;
+        transform.eulerAngles = new Vector3(xRotation, yRotation, 0.0f);
+    }
+}
---
+++ Scenes/TermP_VirtualReality/README.md
@@ -0,0 +1,5 @@
+# TermP_VirtualReality
+## Notice
+1. Eliminate all other unecessary files to keep the repository light.
+2. Doublecheck in Line group chat first before attempting any change.
+3. Pull the latest version before starting to make any change.
