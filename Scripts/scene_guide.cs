a47321ecbf46e62edda877ba5c77ce5c21a6507a
mozium <ChengHsi.Yang@gmail.com>
2020-06-19 17:22:17 +0800

    adjusting objects in classroom and attempting scene shifting mechanisms

---
+++ Scripts/scene_guide.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_guide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "b_portal")
+        {
+            SceneManager.LoadScene("bathroom");
+        }
+        else if (collision.gameObject.tag == "b_back")
+        {
+            SceneManager.LoadScene("classroom");
+        }
+        else if (collision.gameObject.tag == "e_portal")
+        {
+            SceneManager.LoadScene("elavator");
+        }
+        else if (collision.gameObject.tag == "e_back")
+        {
+            SceneManager.LoadScene("classroom");
+        }
+    }
+
+}
