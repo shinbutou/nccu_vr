c500f8b2226ca50898b6ca03accc69dd01b1cc5e
j99120701 <j99120701@yahoo.com.tw>
2020-06-25 17:03:29 +0800

    adding ghost_wall script

---
+++ sadako1.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class sadako1 : MonoBehaviour
+{
+    public GameObject Ghost;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        Ghost.SetActive(true);
+        //sadako1.transform.DOLocalMoveY(1.833f, 0.5f);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Ghost.SetActive(false);
+        //sadako1.transform.DOLocalMoveY(-0.295f, 0f);
+    }
+
+
+
+
+
+}
---
+++ touch_ghost.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class touch_ghost : MonoBehaviour
+{
+    public GameObject ghost;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        ghost.SetActive(true);
+        //sadako1.transform.DOLocalMoveY(1.833f, 0.5f);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        ghost.SetActive(false);
+        //sadako1.transform.DOLocalMoveY(-0.295f, 0f);
+    }
+}
